dc8c82b2cb7bf8000bcc320f97441cf2b2877d81
SebasCarballoo <sebas_arc199@hotmail.com>
2022-06-13 00:31:48 -0600

    commit1

---
+++ TicoAutos/src/main/java/com/ticoautos/TicoautosApplication.java
@@ -0,0 +1,13 @@
+package com.ticoautos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicoautosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicoautosApplication.class, args);
+	}
+
+}
---
+++ TicoAutos/src/test/java/com/ticoautos/TicoautosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticoautos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicoautosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
